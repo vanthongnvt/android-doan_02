c6809c91efac1a5190c924006f2da489b0086df4
vanthongnvt <vanthongnvt1999@gmail.com>
2019-11-07 20:44:57 +0700

    handle btn click login & edit layout login

--- app/src/main/java/com/example/tours/MainActivity.java
+++ app/src/main/java/com/example/tours/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.tours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tours.ApiService.APIRetrofitCreator;
@@ -20,26 +27,88 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  APITour apiTour;
+    private APITour apiTour;
+    private Button btnLogin;
+    private Button btnFbLogin;
+    private Button btnGGLogin;
+    private TextView linkForgotPassword;
+    private TextView linkSignUp;
+    private EditText txtEmailPhone;
+    private EditText txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.login_title_bar);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorCustomPrimary)));
 
         //khoi tao doi tuong de goi api
-        APITour apiTour =  new APIRetrofitCreator().getAPIService();
+        apiTour =  new APIRetrofitCreator().getAPIService();
 
-        String emailPhone = "someemail";
-        String password ="1234";
+        btnLogin=findViewById(R.id.btn_login);
+        btnFbLogin=findViewById(R.id.btn_login_fb);
+        btnGGLogin=findViewById(R.id.btn_login_gg);
+        linkForgotPassword=findViewById(R.id.link_forgot_password);
+        linkSignUp=findViewById(R.id.link_sign_up);
+        txtEmailPhone=findViewById(R.id.login_input_email_phone);
+        txtPassword=findViewById(R.id.login_input_password);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailorPhone = txtEmailPhone.getText().toString();
+                if(emailorPhone.isEmpty()){
+                    Toast.makeText(MainActivity.this, R.string.login_empty_emailphone, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String password = txtPassword.getText().toString();
+                if(password.isEmpty()){
+                    Toast.makeText(MainActivity.this, R.string.login_empty_password, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                normaLogin(emailorPhone,password);
+            }
+        });
+
+        btnFbLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.unavailable_func, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnGGLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.unavailable_func, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        linkSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent itent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(itent);
+            }
+        });
+
+
+
+
+    }
+
+    private void normaLogin(String emailorPhone, String password){
         //goi api, su dung queue de thuc hien tac vu chay nen
-        apiTour.normalLogin(emailPhone,password).enqueue(new Callback<Auth>() {
+        apiTour.normalLogin(emailorPhone,password).enqueue(new Callback<Auth>() {
             @Override
             public void onResponse(Call<Auth> call, Response<Auth> response) {
                 //200 - OK
                 if(response.isSuccessful()){
                     Auth mAuthObject = response.body();
+
+                    //chuyen sang man hinh danh sach tour
                 }
 
                 //400 - 404 - 500
@@ -48,7 +117,8 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         jsonObject = new JSONObject(response.errorBody().string());
                         String message = jsonObject.getString("message");
-                        Toast.makeText(MainActivity.this, message + " ahhi", Toast.LENGTH_SHORT).show();
+
+                        Toast.makeText(MainActivity.this, R.string.login_not_existed, Toast.LENGTH_SHORT).show();
                     } catch (JSONException e) {
                         e.printStackTrace();
                     } catch (IOException e) {
@@ -60,10 +130,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Auth> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Failed to fetch Api", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, R.string.failed_fetch_api, Toast.LENGTH_SHORT).show();
             }
         });
-
-
     }
 }
